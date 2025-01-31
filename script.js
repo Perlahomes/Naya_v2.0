b88026e0ae88479a3f8446fafa6e654c4d693223
Perlahomes <perlagithub@gmail.com>
2025-01-31 15:06:29 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll('img');
-
-// Loop through each image and add an event listener
-images.forEach(image => {
-  image.addEventListener('click', function() {
-    // Toggle the enlarged class on click
-    image.classList.toggle('enlarged');
-  });
-});
